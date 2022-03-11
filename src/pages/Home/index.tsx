c3646a5e5e1ee3906686337d26d3fc3a9d0a78df
Guilherme Couto <gcoutos95@gmail.com>
2022-03-11 19:38:16 -0300

    feat: Adding animations to the homepage

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,8 +1,22 @@
 import { Box, Flex, Image, Text } from "@chakra-ui/react";
+import Hamburger from "hamburger-react";
+import { motion } from "framer-motion";
+
+const FlexMotion = motion(Flex);
+const BoxMotion = motion(Box);
+const ImageMotion = motion(Image);
 
 export const Home = () => {
   return (
     <>
+      <BoxMotion
+        position="absolute"
+        right="0"
+        padding="0 20px"
+        animate={{ opacity: [0, 1] }}
+      >
+        <Hamburger />
+      </BoxMotion>
       <Flex
         w="100vw"
         h="100vh"
@@ -10,7 +24,13 @@ export const Home = () => {
         justifyContent="space-between"
         alignItems="center"
       >
-        <Flex w="100%" h="100%" flexDir="column" alignItems="center">
+        <FlexMotion
+          w="100%"
+          h="100%"
+          flexDir="column"
+          alignItems="center"
+          animate={{ opacity: [0, 1], y: [-10, 0] }}
+        >
           <Box textAlign={["center", "left"]} margin="auto 0">
             <Text
               fontSize={["xl", "2xl", "4xl", "5xl"]}
@@ -35,12 +55,13 @@ export const Home = () => {
               Front-end
             </Text>
           </Box>
-        </Flex>
-        <Image
+        </FlexMotion>
+        <ImageMotion
           src={require("../../assets/bird.png")}
           draggable={false}
           h={["50%", "55%", "65%", "70%", "80%"]}
           alignSelf={["center", "flex-end"]}
+          animate={{ opacity: [0, 1], y: [10, 0] }}
         />
       </Flex>
     </>
